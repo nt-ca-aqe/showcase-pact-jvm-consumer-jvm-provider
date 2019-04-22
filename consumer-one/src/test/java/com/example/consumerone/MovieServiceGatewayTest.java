df751664fcd9e1649a9119a0f4bc62a7a9cde0ad
Antoniya Atanasova <antoniya.atanasova@novatec-gmbh.de>
2019-04-22 16:49:37 +0200

    Workshop set up.

--- consumer-one/src/test/java/com/example/consumerone/MovieServiceGatewayTest.java
+++ consumer-one/src/test/java/com/example/consumerone/MovieServiceGatewayTest.java
@@ -4,20 +4,14 @@ import static au.com.dius.pact.consumer.ConsumerPactRunnerKt.runConsumerTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import au.com.dius.pact.consumer.ConsumerPactBuilder;
 import au.com.dius.pact.consumer.PactTestRun;
 import au.com.dius.pact.consumer.PactVerificationResult;
-import au.com.dius.pact.consumer.dsl.DslPart;
-import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.model.MockProviderConfig;
 import au.com.dius.pact.model.RequestResponsePact;
 
@@ -34,26 +28,8 @@ public class MovieServiceGatewayTest {
     @Test
     public void getSingleMovie() {
 
-        RequestResponsePact pact = ConsumerPactBuilder//
-                .consumer("consumer-one")//
-                .hasPactWith("provider")//
-                .given("Getting movie with any ID returns Iron Man")//
-                .uponReceiving("get single movie")//
-                .path("/movies/b3fc0be8-463e-4875-9629-67921a1e00f4")//
-                .method("GET")//
-                .willRespondWith()//
-                .status(200)//
-                .headers(headers)//
-                .body(new PactDslJsonBody()//
-                        .stringType("title", "Iron Man")//
-                        .numberType("imdbScore", 7.9f))//
-                .toPact();
-
-        executeWithPact(pact, mockServer -> {
+        executeWithPact(null, mockServer -> {
             config.setUrl(mockServer.getUrl());
-            Movie movie = cut.getMovie("b3fc0be8-463e-4875-9629-67921a1e00f4").get();
-            assertThat(movie.getTitle()).isEqualTo("Iron Man");
-            assertThat(movie.getImdbScore()).isEqualTo(7.9f);
         });
 
     }
@@ -61,68 +37,16 @@ public class MovieServiceGatewayTest {
     @Test
     public void getMoviesByScore() {
 
-        float minScore = 8.0f;
-
-        DslPart response = new PactDslJsonBody()//
-                .eachLike("movies")
-                .stringType("title", "Iron Man")//
-                .numberType("imdbScore", minScore)
-                .closeObject();
-
-        RequestResponsePact pact = ConsumerPactBuilder//
-                .consumer("consumer-one")//
-                .hasPactWith("provider")//
-                .given("Getting movies by score returns at least one movie")//
-                .uponReceiving("get movies by score")//
-                .path("/movies")//
-                .query("score=" + minScore)
-                .method("GET")//
-                .willRespondWith()//
-                .status(200)//
-                .headers(headers)//
-                .body(response)//
-                .toPact();
-
-        executeWithPact(pact, mockServer -> {
+        executeWithPact(null, mockServer -> {
             config.setUrl(mockServer.getUrl());
-            List<Movie> movies = cut.getMoviesByScore(minScore);
-            movies.forEach(movie -> {
-                assertThat(movie.getImdbScore()).isGreaterThanOrEqualTo(minScore);
-            });
         });
     }
 
     @Test
     public void getMoviesByInvalidScore() {
 
-        float invalidScore = 11f;
-
-        DslPart response = new PactDslJsonBody()//
-                .numberType("status", 400)
-                .stringType("error", HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .stringType("message", "Received invalid request. Score should be between 1 and 10.");
-
-        RequestResponsePact pact = ConsumerPactBuilder//
-                .consumer("consumer-one")//
-                .hasPactWith("provider")//
-                .given("Getting movies by score invalid score returns bad request")//
-                .uponReceiving("get movies by invalid score")//
-                .path("/movies")//
-                .query("score=" + invalidScore)
-                .method("GET")//
-                .willRespondWith()//
-                .status(400)//
-                .body(response)//
-                .toPact();
-
-        executeWithPact(pact, mockServer -> {
+        executeWithPact(null, mockServer -> {
             config.setUrl(mockServer.getUrl());
-            try {
-                cut.getMoviesByScore(invalidScore);
-            } catch (Exception e) {
-                assertThat(e).isInstanceOf(HttpClientErrorException.class);
-                assertThat(e.getMessage()).isEqualTo("400 Bad Request");
-            }
         });
     }
 
